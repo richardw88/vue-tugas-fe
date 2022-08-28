5972b88edc8e59972fb614e4b648ae89fea3005b
richard <wiryadinatarichard1@gmail.com>
2022-08-28 17:18:26 +0700

    menambahkan fungsi jam

--- src/views/Hello.vue
+++ src/views/Hello.vue
@@ -3,7 +3,9 @@
     <body>
       <div class="bg">
         <div class="back">
-          <router-link to="/"><img src="../assets/back.svg" width="50" height="50"/></router-link>
+          <router-link to="/"
+            ><img src="../assets/back.svg" width="50" height="50"
+          /></router-link>
         </div>
         <div>
           <div class="jam_analog_container">
@@ -25,9 +27,40 @@
 <script>
 export default {
   name: "App",
-  data() {
-    return {};
+  methods: {
+    InitClock() {
+      const secondHand = document.querySelector(".jarum_detik");
+      const minuteHand = document.querySelector(".jarum_menit");
+      const jarum_jam = document.querySelector(".jarum_jam");
+
+      
+        const now = new Date();
+
+        const seconds = now.getSeconds();
+        const secondsDegrees = (seconds / 60) * 360 + 90;
+        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+        if (secondsDegrees === 90) {
+          secondHand.style.transition = "none";
+        } else if (secondsDegrees >= 91) {
+          secondHand.style.transition =
+            "all 0.05s cubic-bezier(0.1, 2.7, 0.58, 1)";
+        }
+
+        const minutes = now.getMinutes();
+        const minutesDegrees = (minutes / 60) * 360 + 90;
+        minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+        const hours = now.getHours();
+        const hoursDegrees = (hours / 12) * 360 + 90;
+        jarum_jam.style.transform = `rotate(${hoursDegrees}deg)`;
+      
+
+    }
   },
+  mounted(){
+    console.log("cek")
+    setInterval( this.InitClock, 1000);
+  }
 };
 </script>
 
@@ -50,8 +83,8 @@ export default {
   padding: 0;
 }
 
-.back{
-  margin:30px;
+.back {
+  margin: 30px;
 }
 
 h1,
